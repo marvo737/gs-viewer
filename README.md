11801ca9c8ecca074d30ea9cdd3da77cdab73dea
marvo737 <marvo.develop@gmail.com>
2023-12-25 09:55:26 +0900

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # gs-viewer
+This is a viewer sample for viewing splat files in a browser.  
+Install node.js and start it with `npx http-server`
---
+++ script/index.js
@@ -0,0 +1,36 @@
+import * as SPLAT from "https://cdn.jsdelivr.net/npm/gsplat@latest";
+
+const canvas = document.getElementById("canvas");
+const progressDialog = document.getElementById("progress-dialog");
+const progressIndicator = document.getElementById("progress-indicator");
+
+const renderer = new SPLAT.WebGLRenderer(canvas);
+const scene = new SPLAT.Scene();
+const camera = new SPLAT.Camera();
+const controls = new SPLAT.OrbitControls(camera, canvas);
+
+async function main() {
+    // const url = "https://huggingface.co/datasets/dylanebert/3dgs/resolve/main/bonsai/bonsai-7k-mini.splat";
+    const filepath = "data/model.splat"
+
+    await SPLAT.Loader.LoadAsync(filepath, scene, (progress) => progressIndicator.value = progress * 100);
+    progressDialog.close();
+
+    const handleResize = () => {
+        renderer.setSize(window.innerWidth, window.innerHeight);
+    };
+
+    const frame = () => {
+        controls.update();
+        renderer.render(scene, camera);
+
+        requestAnimationFrame(frame);
+    };
+
+    handleResize();
+    window.addEventListener("resize", handleResize);
+
+    requestAnimationFrame(frame);
+}
+
+main();
